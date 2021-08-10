046959345f4fc826be94a11129327f476f5d2f6a
AoiNishinaka <crowned021303@gmail.com>
2021-08-10 19:15:12 +0900

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -37,118 +37,199 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
           Container(
             height: 30,
           ),
-          TextButton(onPressed: (){},
-              child: Text(
-                'skip',
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    color: Colors.blue
-                  ),
-              ),),
-          Expanded(child:
-          Container(
-            height: MediaQuery.of(context).size.height / 3,
-            child: PageView(
-              controller: _controller,
-              onPageChanged: (index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
-              children: [
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      Icon(Icons.ac_unit_outlined,
-                      size: 120,),
-                      Text(
-                        'If theyre there,well find em',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                        ),
-                      ),
-                      Text(
-                        'SeatGeek brings together tickets from'
-                            'hundreds of sites to save you time and'
-                            'money',
-                        style: TextStyle(
-                          fontSize: 15,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container (child: Column(
-                children: <Widget>[
-                Icon(Icons.flag,
-                size: 120,),
-                Text(
-                'If theyre there,well find em',
-                style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                ),
-                ),
-                Text(
-                'SeatGeek brings together tickets from'
-                'hundreds of sites to save you time and'
-                'money',
-                style: TextStyle(
-                fontSize: 15,
-                ),
-                ),
-                ],
-                ),
-                ),
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      Expanded(child:
-                      Icon(Icons.backpack,
-                        size: 120,),
-                      ),
-
-                      Text(
-                        'If theyre there,well find em',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                        ),
-                      ),
-                      Text(
-                        'SeatGeek brings together tickets from'
-                            'hundreds of sites to save you time and'
-                            'money',
-                        style: TextStyle(
-                          fontSize: 15,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'skip',
+              style: TextStyle(color: Colors.blue),
             ),
           ),
-          ),
-          SizedBox(height: 18,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              pageDot(0),
-              pageDot(1),
-              pageDot(2),
-            ],
-          ),
+          Expanded(
+            child: Container(
+              child: PageView(
+                  controller: _controller,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _selectedIndex = index;
+                    });
+                  },
+                  children: [
+                    Container(
+                      child: Column(children: <Widget>[
+                        Expanded(
+                          child: Icon(
+                            Icons.flag,
+                            size: 200,
+                          ),
+                        ),
+                        Text(
+                          "If they're there,we'll find 'em",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'SeatGeek brings together tickets from '
+                          '\nhundreds of sites to save you time and '
+                          '\nmoney.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 15,
+                            height: 1.5,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 60,
+                        ),
+                      ]),
+                    ),
+                    //ここまでが１ページ
+                    Container(
+                      child: Column(children: <Widget>[
+                        Expanded(
+                          child: Icon(
+                            Icons.mail,
+                            size: 200,
+                          ),
+                        ),
+                        Text(
+                          "If they're there,we'll find 'em",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'SeatGeek brings together tickets from '
+                          '\nhundreds of sites to save you time and '
+                          '\nmoney.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 15,
+                            height: 1.5,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 60,
+                        ),
+                      ]),
+                    ),
+                    //ここまでが2ページ
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: Icon(
+                              Icons.backpack,
+                              size: 200,
+                            ),
+                          ),
 
-        ],
-      ),
-    );
+                          Text(
+                            "If they're there,we'll find 'em",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            'SeatGeek brings together tickets from '
+                            '\nhundreds of sites to save you time and '
+                            '\nmoney.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 15,
+                              height: 1.5,
+                              color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 60,
+                        ),
+                      ],
+                    ),
+                  ),
+                ])),
+          ),
+          //ここまでが３ページ
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        pageDot(0),
+                        pageDot(1),
+                        pageDot(2),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Sign up',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Log in',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]
+
+          ));
   }
 }
